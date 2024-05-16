c2afcdec18c8fb1ab15e110d961465a0bdcd5b67
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-05-16 21:52:32 +0100

    commit

--- server.js
+++ server.js
@@ -2,6 +2,11 @@
 const express = require('express')
 const app = express()
 
+
 app.listen(3000, function () {
     console.log('listening on port 3000')
+})
+
+app.get('/', function (req, res) { 
+    res.send('Hello, world')
 })
