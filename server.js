cadb25aa8bfc33108c828c32b00ad20e69173eb3
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-05-16 22:23:40 +0100

    commit

--- server.js
+++ server.js
@@ -1,22 +1,17 @@
 // console.log('Running')
 const express = require('express')
 const app = express()
-const figlet = require('figlet')
+// const figlet = require('figlet')
 
-figlet('404', (err, data) => {
-    if (err) {
-        console.log('something went wrong....')
-        console.dir(err)
-        return;
-    }
-    console.log(data)
-})
 
 app.get('/', (req, res)=> { 
-    res.send(_dirname + 'index.html')
+    res.sendFile(__dirname + '/index.html')
+    // console.log(req.headers); 
 })
 
 app.listen(3000,  ()=> {
     console.log('listening on port 3000')
 })
 
+
+// Outputs: 'GET'
