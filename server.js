2e9bf51f5413951e1f0ccc9db5a879aaf1554a4b
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-05-16 22:55:32 +0100

    commit

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 // console.log('Running')
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 // const figlet = require('figlet')
 
+//Make sure you place body-parser before CRUD handler
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res)=> { 
     res.sendFile(__dirname + '/index.html') // console.log(req.headers); 
 })
 
 app.post('/quotes', (req, res)=> {
-    console.log('Hello world')
+    console.log(req.body)
 })
 
 app.listen(3000,  ()=> {
