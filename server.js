61f2fd16e0ffd9df1ed3e92c406081443a05e637
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-05-17 16:33:13 +0100

    commit

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 const app = express()
 // const figlet = require('figlet')
+// const connectionString =  'mongodb+sr://djsmacker01:Fabulous01@cluster0.pxo3cst.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0' 
+MongoClient.connect(connectionString, (err, client) => {
+    
+    if (err) return console.log(err)
+    console.log('connected to database')
 
-//Make sure you place body-parser before CRUD handler
-app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res)=> { 
     res.sendFile(__dirname + '/index.html') // console.log(req.headers); 
@@ -20,12 +24,10 @@ app.listen(3000,  ()=> {
     console.log('listening on port 3000')
 })
 
-// MongoClient.connect('mongodb-connection-string', (err, client) => {
-//     // 'mongodb+srv://djsmacker01:Fabulous01@cluster0.pxo3cst.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-    
-//     if (err) return console.log(err)
-//     console.log('connected to database')
-// })
+})
+//Make sure you place body-parser before CRUD handler
+
+
 
 // MongoClient.connect(
 //   connectionString,
