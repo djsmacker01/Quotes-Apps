07689a95a4568212a7e310a07427fcfc75d8c55e
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-05-20 10:44:30 +0100

    commit

--- rap-names-api/server.js
+++ rap-names-api/server.js
@@ -2,8 +2,18 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+const savage = {
+    'age': 29,
+    'birthName': ' Sheyaa Bin John',
+    'birthLocation': 'London, UK',
+}
+
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api', (req, res) => { 
+    res.json()
 })
 
 app.listen(PORT, () => {
