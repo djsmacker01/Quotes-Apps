6a2daeac66fa651969c5c761f3d05ea2690e8fa8
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-05-20 10:36:16 +0100

    commit

--- rap-names-api/server.js
+++ rap-names-api/server.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
+const PORT = 3000
 
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '/index.html')
+})
+
+app.listen(PORT, () => {
+    console.log(`The server is running on ${PORT}!`)
 })
