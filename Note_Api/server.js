d961d2d30dd9c1f460956c70913fb54234fb8a7f
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-05-26 11:01:34 +0100

    Add json files

--- Note_Api/server.js
+++ Note_Api/server.js
@@ -3,7 +3,21 @@ const app = express()
 const PORT = 3000
 
 let notes = [
-    
+    {
+    id: 1,
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
 ]
 
 app.get('/', (req, res) => {
