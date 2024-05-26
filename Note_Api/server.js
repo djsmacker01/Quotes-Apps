9e004b751c8c8b32993ec8dfae14d463d978592e
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-05-26 11:00:52 +0100

    new updates

--- Note_Api/server.js
+++ Note_Api/server.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const app = express()
+const PORT = 3000
+
+let notes = [
+    
+]
 
 app.get('/', (req, res) => {
-    
+    res.send('<h1> Hello, Welcome to Note Application: </br> Where you take note of all languages</h1>')
+})
+
+app.get('/api/notes', (req, res) => { 
+res.json(notes)
+})
+
+app.listen(PORT,() => {
+ console.log(`Server is running on Port:  ${PORT}`)
 })
